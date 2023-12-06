800d31de990766ff90049847aeba9be6328b4a52
Anton Prokhorov <prokhorov.anton.s@gmail.com>
2023-12-06 23:59:16 +0000

    day1 part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,11 +1,10 @@
 use std::{fs::File, io::BufRead, io::BufReader};
 
-fn main() {
+fn part1() -> Result<u32, String> {
     let file = match File::open("./files/input.txt") {
         Ok(file) => file,
         Err(err) => {
-            eprintln!("Error: {:#?}", err);
-            std::process::exit(1);
+            return Err(err.to_string());
         }
     };
 
@@ -18,8 +17,7 @@ fn main() {
         numbers = (None, None);
 
         if let Err(err) = line {
-            eprintln!("Error: {:#?}", err);
-            std::process::exit(1);
+            return Err(err.to_string());
         }
 
         let line = line.unwrap();
@@ -42,5 +40,77 @@ fn main() {
 
         sum += 10 * numbers.0.unwrap() + numbers.1.unwrap();
     }
-    println!("Answer: {}", sum)
+    return Ok(sum);
+}
+
+fn part2() -> Result<u32, String> {
+    let file = match File::open("./files/input.txt") {
+        Ok(file) => file,
+        Err(err) => {
+            return Err(err.to_string());
+        }
+    };
+
+    let lines = BufReader::new(file).lines();
+    let words = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    let mut numbers: (Option<(usize, u32)>, Option<(usize, u32)>);
+    let mut sum: u32 = 0;
+
+    for line in lines {
+        numbers = (None, None);
+
+        if let Err(err) = line {
+            return Err(err.to_string());
+        }
+
+        let line = line.unwrap();
+
+        for (i, c) in line.chars().enumerate() {
+            if c.is_ascii_digit() {
+                numbers.0 = Some((i, c.to_digit(10).unwrap()));
+                break;
+            }
+        }
+
+        for (i, c) in line.chars().rev().enumerate() {
+            if c.is_ascii_digit() {
+                numbers.1 = Some((line.len() - i - 1, c.to_digit(10).unwrap()));
+                break;
+            }
+        }
+
+        for (i, word) in words.iter().enumerate() {
+            for (j, _) in line.match_indices(word).into_iter() {
+                let v = (i + 1) as u32;
+                match numbers {
+                    (Some(first), Some(last)) => {
+                        if j < first.0 {
+                            numbers.0 = Some((j, v));
+                        }
+                        if j > last.0 {
+                            numbers.1 = Some((j, v));
+                        }
+                    }
+                    (None, None) => {
+                        numbers.0 = Some((j, v));
+                        numbers.1 = Some((j, v));
+                    }
+                    (_, _) => {
+                        todo!()
+                    }
+                }
+            }
+        }
+
+        sum += 10 * numbers.0.unwrap().1 + numbers.1.unwrap().1;
+    }
+    return Ok(sum);
+}
+
+fn main() {
+    println!("Part 1: {}", part1().unwrap());
+    println!("Part 2: {}", part2().unwrap());
 }
