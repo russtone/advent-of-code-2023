374c5b30f580bde11a27f951df95f5d180f63ef7
Anton Prokhorov <prokhorov.anton.s@gmail.com>
2023-12-09 21:05:58 +0000

    day3 rewrite part1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -12,115 +12,122 @@ fn main() {
         }
     };
 
-    let lines = BufReader::new(file).lines();
-    let mut pos: Option<Pos>;
+    let mut lines = BufReader::new(file).lines().enumerate().peekable();
+    let mut num_pos: Option<Pos>;
     let mut sum: u32 = 0;
-    let mut last_line_num: Vec<Num> = Vec::new();
-    let mut this_line_num: Vec<Num> = Vec::new();
-    let mut last_line_sym: Vec<usize> = Vec::new();
-    let mut this_line_sym: Vec<usize> = Vec::new();
-    let mut last_sym: Option<usize>;
+    let mut nums: Vec<Num> = Vec::new();
+    let mut syms: Vec<Sym> = Vec::new();
 
-    for line in lines {
+    while let Some((i, line)) = lines.next() {
         let line = line.unwrap();
-        pos = None;
-        last_sym = None;
+        num_pos = None;
 
         for (j, c) in line.chars().enumerate() {
             match c {
-                '0'..='9' => match pos {
+                '0'..='9' => match num_pos {
                     Some(ref mut pos) => pos.end = j + 1,
-                    None => pos = Some(Pos::new(j, j + 1)),
+                    None => num_pos = Some(Pos::new(i, j, j + 1)),
                 },
                 _ => {
                     match c {
                         '.' => {}
-                        _ => {
-                            this_line_sym.push(j);
-                            last_sym = Some(j);
+                        _ => syms.push(Sym::new(i, j)),
+                    }
+                    parse_and_save(&line, &num_pos, &mut nums);
+                    num_pos = None
+                }
+            }
+        }
+        parse_and_save(&line, &num_pos, &mut nums);
+
+        if i >= 2 {
+            for num in nums.iter_mut() {
+                for sym in syms.iter_mut() {
+                    if is_adjacent(sym, num) {
+                        sym.adjacent_nums_count += 1;
+                        match sym.first_two_adjacent_nums {
+                            (None, None) => sym.first_two_adjacent_nums = (Some(num.value), None),
+                            (Some(n1), None) => {
+                                sym.first_two_adjacent_nums = (Some(n1), Some(num.value))
+                            }
+                            _ => {}
                         }
-                    }
-                    if let Some(p) = pos {
-                        process_this_line_num(&line, p, last_sym, &mut sum, &mut this_line_num);
-                        pos = None
+                        num.is_inclued = true;
                     }
                 }
             }
-        }
 
-        if let Some(p) = pos {
-            process_this_line_num(&line, p, last_sym, &mut sum, &mut this_line_num)
-        }
-
-        'outer: for num in last_line_num.iter() {
-            for sym in this_line_sym.iter() {
-                if is_near(sym, &num.pos) {
-                    sum += num.value;
-                    continue 'outer;
-                }
+            for num in nums
+                .iter()
+                .filter(|n| (n.pos.row == (i - 2) || lines.peek().is_none()) && n.is_inclued)
+            {
+                sum += num.value;
             }
+            nums.retain(|n| n.pos.row > (i - 2));
         }
-
-        last_line_num.clear();
-
-        'outer: for num in this_line_num.iter() {
-            for sym in last_line_sym.iter() {
-                if is_near(sym, &num.pos) {
-                    sum += num.value;
-                    continue 'outer;
-                }
-            }
-            last_line_num.push(*num)
-        }
-
-        last_line_sym = this_line_sym.to_owned();
-        this_line_num.clear();
-        this_line_sym.clear();
     }
 
     println!("Answer: {}", sum)
 }
 
-fn process_this_line_num(
-    line: &str,
-    pos: Pos,
-    last_sym: Option<usize>,
-    sum: &mut u32,
-    nums: &mut Vec<Num>,
-) {
-    let s = &line[pos.start..pos.end];
-    let n: u32 = s.parse().unwrap();
-    if last_sym.is_some() && is_near(&last_sym.unwrap(), &pos) {
-        *sum += n
-    } else {
-        nums.push(Num::new(pos, n));
+fn parse_and_save(line: &str, pos: &Option<Pos>, nums: &mut Vec<Num>) {
+    if let Some(pos) = pos {
+        let s = &line[pos.start..pos.end];
+        let n: u32 = s.parse().unwrap();
+        nums.push(Num::new(*pos, n));
     }
 }
 
-fn is_near(i: &usize, pos: &Pos) -> bool {
-    *i + 1 >= pos.start && *i <= pos.end
+fn is_adjacent(sym: &Sym, num: &Num) -> bool {
+    sym.pos.row + 1 >= num.pos.row
+        && sym.pos.row <= num.pos.row + 1
+        && sym.pos.start + 1 >= num.pos.start
+        && sym.pos.start <= num.pos.end
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Pos {
+    row: usize,
+    start: usize,
+    end: usize,
+}
+
+impl Pos {
+    fn new(row: usize, start: usize, end: usize) -> Pos {
+        return Pos { row, start, end };
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
 struct Num {
     pos: Pos,
     value: u32,
+    is_inclued: bool,
 }
 
 impl Num {
     fn new(pos: Pos, value: u32) -> Num {
-        Num { pos, value }
+        Num {
+            pos,
+            value,
+            is_inclued: false,
+        }
     }
 }
 
 #[derive(Debug, Clone, Copy)]
-struct Pos {
-    start: usize,
-    end: usize,
+struct Sym {
+    pos: Pos,
+    adjacent_nums_count: u32,
+    first_two_adjacent_nums: (Option<u32>, Option<u32>),
 }
 
-impl Pos {
-    fn new(start: usize, end: usize) -> Pos {
-        Pos { start, end }
+impl Sym {
+    fn new(row: usize, col: usize) -> Sym {
+        Sym {
+            pos: Pos::new(row, col, col),
+            adjacent_nums_count: 0,
+            first_two_adjacent_nums: (None, None),
+        }
     }
 }
