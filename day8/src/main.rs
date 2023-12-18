28b5da1eae2f65d1de52e23bd635e92825d5af5c
Anton Prokhorov <prokhorov.anton.s@gmail.com>
2023-12-18 23:24:24 +0000

    day8: part2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -25,6 +25,13 @@ fn main() -> Result<(), Error> {
         return Err(Error::EmptyCommands);
     }
 
+    println!("{}", part1(&map, &commands)?);
+    println!("{}", part2(&map, &commands)?);
+
+    Ok(())
+}
+
+fn part1(map: &HashMap<String, (String, String)>, commands: &Vec<Command>) -> Result<u32, Error> {
     let mut key: String = "AAA".to_owned();
     let mut cmd_index: usize = 0;
     let mut steps: u32 = 0;
@@ -40,9 +47,57 @@ fn main() -> Result<(), Error> {
         steps += 1;
     }
 
-    println!("{}", steps);
+    Ok(steps)
+}
 
-    Ok(())
+fn part2(map: &HashMap<String, (String, String)>, commands: &Vec<Command>) -> Result<u64, Error> {
+    let keys: Vec<String> = map
+        .clone()
+        .into_keys()
+        .filter(|key| key.ends_with("A"))
+        .collect();
+
+    let mut cmd_index: usize = 0;
+    let mut steps: Vec<u64> = vec![0; keys.len()];
+
+    for (i, key) in keys.iter().enumerate() {
+        let mut key: String = key.to_owned();
+        let step = &mut steps[i];
+        while !key.ends_with("Z") {
+            let cmd = &commands[cmd_index % commands.len()];
+            let values = map.get(&key).unwrap();
+            match cmd {
+                Command::Left => key = values.0.to_owned(),
+                Command::Right => key = values.1.to_owned(),
+            }
+            cmd_index += 1;
+            *step += 1;
+        }
+    }
+
+    let mut res = steps[0];
+
+    for i in 1..steps.len() {
+        res = lcm(res, steps[i]);
+    }
+
+    Ok(res)
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    (a * b) / gcd(a, b)
+}
+
+fn gcd(a: u64, b: u64) -> u64 {
+    let mut a = a;
+    let mut b = b;
+    while b != 0 {
+        if b < a {
+            std::mem::swap(&mut b, &mut a);
+        }
+        b %= a;
+    }
+    a
 }
 
 #[derive(Debug)]
