73db7718c6719465e3ba95b998905340ca410b62
Anton Prokhorov <prokhorov.anton.s@gmail.com>
2023-12-25 17:54:38 +0000

    day10: part2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -6,7 +6,7 @@ use std::{
 
 fn main() -> Result<(), Error> {
     let file = File::open("files/input.txt")?;
-    let mut lines: Vec<String> = BufReader::new(file).lines().collect::<Result<_, _>>()?;
+    let lines: Vec<String> = BufReader::new(file).lines().collect::<Result<_, _>>()?;
 
     let map = Map::new(lines);
 
@@ -36,7 +36,16 @@ fn main() -> Result<(), Error> {
         steps += 1;
     }
 
-    println!("{}", steps);
+    println!("Steps: {}", steps);
+
+    let mut count = 0;
+    for p in MapIterator::new(&map) {
+        if map.is_inside(&seen, &p) {
+            count += 1;
+        }
+    }
+
+    println!("Count: {}", count);
 
     Ok(())
 }
@@ -116,6 +125,193 @@ impl Map {
     fn get_char_at(&self, row: usize, col: usize) -> Option<char> {
         self.data.get(row)?.chars().nth(col)
     }
+
+    fn get_point_at(&self, row: usize, col: usize) -> Option<Point> {
+        Some(Point::new(row, col, self.get_char_at(row, col)?))
+    }
+
+    fn is_inside(&self, points: &BTreeSet<Point>, p: &Point) -> bool {
+        if points.contains(p) {
+            return false;
+        }
+
+        let directions = vec![p.col, p.row, self.cols - p.col, self.rows - p.row];
+        let m = directions.iter().enumerate().min_by_key(|x| x.1).unwrap();
+
+        let mut n = 0;
+        let mut path: Vec<Point> = Vec::new();
+
+        match m {
+            (0, _) => {
+                for col in 0..p.col {
+                    if let Some(q) = self.get_point_at(p.row, col) {
+                        if points.contains(&q) {
+                            if let Some(r) = path.last() {
+                                match (r.char, q.char) {
+                                    ('S' | '-' | 'F' | 'L', '-' | '7' | 'J') => {
+                                        path.push(q);
+                                        continue;
+                                    }
+                                    _ => {
+                                        match path.as_slice() {
+                                            [first, .., last] => match (first.char, last.char) {
+                                                ('F', '7') | ('L', 'J') => n += 2,
+                                                ('L', '7') | ('F', 'J') => n += 1,
+                                                _ => panic!("error"),
+                                            },
+                                            [_] => n += 1,
+                                            _ => {}
+                                        }
+                                        path.clear();
+                                        path.push(q);
+                                        continue;
+                                    }
+                                }
+                            } else {
+                                path.push(q)
+                            }
+                        }
+                    }
+                }
+                match path.as_slice() {
+                    [first, .., last] => match (first.char, last.char) {
+                        ('F', '7') | ('L', 'J') => n += 2,
+                        ('L', '7') | ('F', 'J') => n += 1,
+                        _ => panic!("error"),
+                    },
+                    [_] => n += 1,
+                    _ => {}
+                }
+            }
+            (1, _) => {
+                for row in 0..p.row {
+                    if let Some(q) = self.get_point_at(row, p.col) {
+                        if points.contains(&q) {
+                            if let Some(r) = path.last() {
+                                match (r.char, q.char) {
+                                    ('S' | '|' | '7' | 'F', '|' | 'L' | 'J') => {
+                                        path.push(q);
+                                        continue;
+                                    }
+                                    _ => {
+                                        match path.as_slice() {
+                                            [first, .., last] => match (first.char, last.char) {
+                                                ('F', 'L') | ('7', 'J') => n += 2,
+                                                ('F', 'J') | ('7', 'L') => n += 1,
+                                                _ => panic!("error"),
+                                            },
+                                            [_] => n += 1,
+                                            _ => {}
+                                        }
+                                        path.clear();
+                                        path.push(q);
+                                        continue;
+                                    }
+                                }
+                            } else {
+                                path.push(q);
+                            }
+                        }
+                    }
+                }
+                match path.as_slice() {
+                    [first, .., last] => match (first.char, last.char) {
+                        ('F', 'L') | ('7', 'J') => n += 2,
+                        ('F', 'J') | ('7', 'L') => n += 1,
+                        _ => panic!("error"),
+                    },
+                    [_] => n += 1,
+                    _ => {}
+                }
+            }
+            (2, _) => {
+                path.clear();
+                for col in p.col + 1..self.cols {
+                    if let Some(q) = self.get_point_at(p.row, col) {
+                        if points.contains(&q) {
+                            if let Some(r) = path.last() {
+                                match (r.char, q.char) {
+                                    ('S' | '-' | 'F' | 'L', '-' | '7' | 'J') => {
+                                        path.push(q);
+                                        continue;
+                                    }
+                                    _ => {
+                                        match path.as_slice() {
+                                            [first, .., last] => match (first.char, last.char) {
+                                                ('F', '7') | ('L', 'J') => n += 2,
+                                                ('L', '7') | ('F', 'J') => n += 1,
+                                                _ => panic!("error"),
+                                            },
+                                            [_] => n += 1,
+                                            _ => {}
+                                        }
+                                        path.clear();
+                                        path.push(q);
+                                        continue;
+                                    }
+                                }
+                            } else {
+                                path.push(q)
+                            }
+                        }
+                    }
+                }
+                match path.as_slice() {
+                    [first, .., last] => match (first.char, last.char) {
+                        ('F', '7') | ('L', 'J') => n += 2,
+                        ('L', '7') | ('F', 'J') => n += 1,
+                        _ => panic!("error"),
+                    },
+                    [_] => n += 1,
+                    _ => {}
+                }
+            }
+            (3, _) => {
+                for row in p.row + 1..self.rows {
+                    if let Some(q) = self.get_point_at(row, p.col) {
+                        if points.contains(&q) {
+                            if let Some(r) = path.last() {
+                                match (r.char, q.char) {
+                                    ('S' | '|' | '7' | 'F', '|' | 'L' | 'J') => {
+                                        path.push(q);
+                                        continue;
+                                    }
+                                    _ => {
+                                        match path.as_slice() {
+                                            [first, .., last] => match (first.char, last.char) {
+                                                ('F', 'L') | ('7', 'J') => n += 2,
+                                                ('F', 'J') | ('7', 'L') => n += 1,
+                                                _ => panic!("error"),
+                                            },
+                                            [_] => n += 1,
+                                            _ => {}
+                                        }
+                                        path.clear();
+                                        path.push(q);
+                                        continue;
+                                    }
+                                }
+                            } else {
+                                path.push(q);
+                            }
+                        }
+                    }
+                }
+                match path.as_slice() {
+                    [first, .., last] => match (first.char, last.char) {
+                        ('F', 'L') | ('7', 'J') => n += 2,
+                        ('F', 'J') | ('7', 'L') => n += 1,
+                        _ => panic!("error"),
+                    },
+                    [_] => n += 1,
+                    _ => {}
+                }
+            }
+            (_, _) => panic!("must never happen"),
+        }
+
+        n % 2 == 1
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
@@ -139,6 +335,42 @@ enum Error {
 impl From<io::Error> for Error {
     fn from(value: io::Error) -> Self {
         Error::Io(value)
+    }
+}
+
+#[derive(Debug)]
+struct MapIterator<'a> {
+    map: &'a Map,
+    row: usize,
+    col: usize,
+}
+
+impl<'a> MapIterator<'a> {
+    fn new(map: &'a Map) -> Self {
+        MapIterator {
+            map,
+            row: 0,
+            col: 0,
+        }
+    }
+}
+
+impl<'a> Iterator for MapIterator<'a> {
+    type Item = Point;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.col < self.map.cols - 1 {
+            self.col += 1;
+            self.map.get_point_at(self.row, self.col)
+        } else {
+            if self.row < self.map.rows - 1 {
+                self.row += 1;
+                self.col = 1;
+                self.map.get_point_at(self.row, self.col)
+            } else {
+                None
+            }
+        }
     }
 }
 
