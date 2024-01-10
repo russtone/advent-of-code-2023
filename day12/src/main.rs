77cbd78b31dc587bff4df321fa92ac4ac06d15db
Anton Prokhorov <prokhorov.anton.s@gmail.com>
2024-01-10 21:55:19 +0000

    day12: part2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,118 +1,250 @@
 use core::num;
-use std::{
-    fs::File,
-    io::{self, BufRead, BufReader},
-    str::FromStr,
-};
+use std::io::{self, BufRead};
 
 fn main() -> Result<(), Error> {
-    let file = File::open("files/input.txt")?;
-    let mut lines: Vec<Line> = Vec::new();
-
-    for s in BufReader::new(file).lines() {
-        lines.push(s?.parse()?)
-    }
-
-    let mut res = 0;
-
-    for line in &lines {
-        let max = 2_u32.pow(line.unknown.len() as u32);
-        let mut val;
-
-        for i in 0..max {
-            val = line.data;
-            for (j, pos) in line.unknown.iter().enumerate() {
-                if i & (1 << j) != 0 {
-                    val |= 1 << pos;
-                }
-            }
-            if checksum(val) == line.checksum {
-                res += 1;
-            }
-        }
-    }
-
-    println!("{}", res);
+    println!("Part 1: {}", part1::solve()?);
+    println!("Part 2: {}", part2::solve()?);
 
     Ok(())
 }
+mod part2 {
 
-fn checksum(n: u32) -> Vec<u32> {
-    let mut res = Vec::new();
-    let mut m: u32 = n;
-    let mut x = 0;
+    use super::*;
+    use core::hash::Hash;
+    use std::{
+        cmp,
+        collections::{hash_map::DefaultHasher, HashMap},
+        fs::File,
+        hash::Hasher,
+        io::BufReader,
+        str::FromStr,
+    };
 
-    while m != 0 {
-        if m & 0x10000000 != 0 {
-            x += 1
-        } else {
-            if x != 0 {
-                res.push(x);
-                x = 0
+    pub fn solve() -> Result<u64, Error> {
+        let file = File::open("files/input.txt")?;
+        let mut lines: Vec<Line> = Vec::new();
+
+        for s in BufReader::new(file).lines() {
+            lines.push(s?.parse()?)
+        }
+
+        let mut res = 0;
+
+        for (i, line) in lines.iter().enumerate() {
+            let mut c = Counter::new();
+            res += c.count(&line.data, &line.checksum);
+        }
+        Ok(res)
+    }
+
+    fn hash_args<T: Hash + ?Sized, U: Hash + ?Sized>(a: &T, b: &U) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        a.hash(&mut hasher);
+        b.hash(&mut hasher);
+        hasher.finish()
+    }
+
+    struct Counter {
+        cache: HashMap<u64, u64>,
+    }
+
+    impl Counter {
+        fn new() -> Self {
+            Counter {
+                cache: HashMap::new(),
             }
         }
-        m <<= 1;
+
+        fn count(&mut self, data: &[char], checksum: &[usize]) -> u64 {
+            let key = hash_args(data, checksum);
+
+            match self.cache.get(&key) {
+                Some(res) => *res,
+                None => {
+                    let res = match (data, checksum) {
+                        ([..], []) => {
+                            if data.contains(&'#') {
+                                0
+                            } else {
+                                1
+                            }
+                        }
+                        (_, [head, tail @ ..]) => {
+                            let max_offset =
+                                data.len() - (tail.iter().sum::<usize>() + head + tail.len() - 1);
+                            let mut res = 0;
+                            for start in 0..max_offset {
+                                let end = start + head;
+
+                                if data[..start].contains(&'#') {
+                                    break;
+                                }
+
+                                if end <= data.len()
+                                    && !data[start..end].contains(&'.')
+                                    && data.get(end) != Some(&'#')
+                                {
+                                    res += self.count(&data[cmp::min(end + 1, data.len())..], &tail)
+                                }
+                            }
+                            res
+                        }
+                    };
+                    self.cache.insert(key, res);
+                    res
+                }
+            }
+        }
     }
-    if x != 0 {
-        res.push(x)
+
+    #[derive(Debug)]
+    struct Line {
+        data: Vec<char>,
+        checksum: Vec<usize>,
     }
 
-    res
-}
+    impl FromStr for Line {
+        type Err = Error;
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let (left, right) = s.split_once(" ").ok_or(Error::ParseLine)?;
 
-trait Pad {
-    fn pad_left(&self, width: usize, char: char) -> String;
-}
+            let mut checksum = Vec::new();
+            for ns in right.split(",") {
+                checksum.push(ns.parse()?)
+            }
+            checksum = checksum.repeat(5);
 
-impl Pad for &str {
-    fn pad_left(&self, width: usize, char: char) -> String {
-        let padding_needed = width.saturating_sub(self.len());
-        let padding = char.to_string().repeat(padding_needed);
+            let mut data: Vec<char> = left.chars().collect();
+            data.push('?');
+            data = data.repeat(5);
+            data.pop();
 
-        format!("{}{}", padding, self)
+            Ok(Line { data, checksum })
+        }
     }
 }
 
-#[derive(Debug)]
-struct Line {
-    data: u32,
-    checksum: Vec<u32>,
-    unknown: Vec<u32>,
-}
+mod part1 {
+    use super::*;
+    use std::{fs::File, io::BufReader, str::FromStr};
 
-impl FromStr for Line {
-    type Err = Error;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (left, right) = s.split_once(" ").ok_or(Error::ParseLine)?;
+    pub fn solve() -> Result<u32, Error> {
+        let file = File::open("files/input.txt")?;
+        let mut lines: Vec<Line> = Vec::new();
 
-        let mut checksum = Vec::new();
-        for ns in right.split(",") {
-            checksum.push(ns.parse()?)
+        for s in BufReader::new(file).lines() {
+            lines.push(s?.parse()?)
         }
 
-        let s = left.pad_left(32, '.');
+        let mut res = 0;
 
-        let data = s
-            .chars()
-            .map(|ch| match ch {
-                '.' | '?' => 0,
-                _ => 1,
+        for line in &lines {
+            let max = 2_u32.pow(line.unknown.len() as u32);
+            let mut val;
+
+            for i in 0..max {
+                val = line.data;
+                for (j, pos) in line.unknown.iter().enumerate() {
+                    if i & (1 << j) != 0 {
+                        val |= 1 << pos;
+                    }
+                }
+                if checksum(val) == line.checksum {
+                    res += 1;
+                }
+            }
+        }
+        Ok(res)
+    }
+
+    fn checksum(n: u32) -> Vec<u32> {
+        let mut res = Vec::new();
+        let mut m: u32 = n;
+        let mut x = 0;
+
+        while m != 0 {
+            if m & 0x10000000 != 0 {
+                x += 1
+            } else {
+                if x != 0 {
+                    res.push(x);
+                    x = 0
+                }
+            }
+            m <<= 1;
+        }
+        if x != 0 {
+            res.push(x)
+        }
+
+        res
+    }
+
+    #[derive(Debug)]
+    struct Line {
+        data: u32,
+        checksum: Vec<u32>,
+        unknown: Vec<u32>,
+    }
+
+    impl FromStr for Line {
+        type Err = Error;
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let (left, right) = s.split_once(" ").ok_or(Error::ParseLine)?;
+
+            let mut checksum = Vec::new();
+            for ns in right.split(",") {
+                checksum.push(ns.parse()?)
+            }
+
+            let s = left.pad_left(32, '.');
+
+            let data = s
+                .chars()
+                .map(|ch| match ch {
+                    '.' | '?' => 0,
+                    _ => 1,
+                })
+                .fold(0, |acc, bit| (acc << 1) | bit);
+
+            let unknown = s
+                .chars()
+                .rev()
+                .enumerate()
+                .filter(|(_, ch)| *ch == '?')
+                .map(|(i, _)| i as u32)
+                .collect();
+
+            Ok(Line {
+                data,
+                checksum,
+                unknown,
             })
-            .fold(0, |acc, bit| (acc << 1) | bit);
+        }
+    }
 
-        let unknown = s
-            .chars()
-            .rev()
-            .enumerate()
-            .filter(|(_, ch)| *ch == '?')
-            .map(|(i, _)| i as u32)
-            .collect();
+    trait Pad {
+        fn pad_left(&self, width: usize, char: char) -> String;
+    }
 
-        Ok(Line {
-            data,
-            checksum,
-            unknown,
-        })
+    impl Pad for &str {
+        fn pad_left(&self, width: usize, char: char) -> String {
+            let padding_needed = width.saturating_sub(self.len());
+            let padding = char.to_string().repeat(padding_needed);
+
+            format!("{}{}", padding, self)
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_checksum() {
+            assert_eq!(checksum(7), vec![3]);
+            assert_eq!(checksum(16190), vec![6, 5]);
+        }
     }
 }
 
@@ -132,16 +264,5 @@ impl From<io::Error> for Error {
 impl From<num::ParseIntError> for Error {
     fn from(value: num::ParseIntError) -> Self {
         Error::ParseInt(value)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_checksum() {
-        assert_eq!(checksum(7), vec![3]);
-        assert_eq!(checksum(16190), vec![6, 5]);
     }
 }
