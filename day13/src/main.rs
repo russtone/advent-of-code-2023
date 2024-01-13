e7c2cef7ea3bc07b52d0b2d49d8f9d0331b88169
Anton Prokhorov <prokhorov.anton.s@gmail.com>
2024-01-13 23:26:07 +0000

    day13: part2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,7 +1,5 @@
 use core::num;
 use std::{
-    cmp::min,
-    collections::HashSet,
     fmt::Display,
     fs::File,
     io::{self, BufRead, BufReader},
@@ -27,67 +25,143 @@ fn main() -> Result<(), Error> {
         }
     }
 
-    let mut res: usize = 0;
-
-    for map in maps.iter() {
-        let mut cols: HashSet<usize> = HashSet::new();
-
-        for (i, row) in map.rows().enumerate() {
-            if i == 0 {
-                cols = find_possible_reflection_offsets(row);
-            } else {
-                cols.retain(|offset| check_reflection(row, *offset));
-            }
-        }
-
-        for col in cols.iter() {
-            res += col + 1;
-        }
-
-        let mut rows: HashSet<usize> = HashSet::new();
-
-        for (i, col) in map.cols().enumerate() {
-            if i == 0 {
-                rows = find_possible_reflection_offsets(&col);
-            } else {
-                rows.retain(|offset| check_reflection(&col, *offset));
-            }
-        }
-
-        for row in rows.iter() {
-            res += (row + 1) * 100;
-        }
-
-        if cols.is_empty() && rows.is_empty() {
-            panic!("no reflection found:\n{}", map);
-        }
-    }
-
-    println!("{}", res);
+    println!("Part1: {}", part1::solve(&maps)?);
+    println!("Part2: {}", part2::solve(&maps)?);
 
     Ok(())
 }
 
-fn find_possible_reflection_offsets(s: &Vec<char>) -> HashSet<usize> {
-    let mut offsets = HashSet::new();
+mod part2 {
+    use std::{cmp::min, iter::zip};
 
-    for offset in 0..s.len() - 1 {
-        if check_reflection(s, offset) {
-            offsets.insert(offset);
+    use super::*;
+
+    pub fn solve(maps: &Vec<Map>) -> Result<usize, Error> {
+        let mut res: usize = 0;
+
+        for map in maps.iter() {
+            let rows = &map.data;
+            let cols: Vec<Vec<char>> = map.cols().collect();
+
+            if let Some(row) = find_offset(rows) {
+                res += (row + 1) * 100;
+            } else if let Some(col) = find_offset(&cols) {
+                res += col + 1;
+            } else {
+                panic!("error");
+            }
         }
+        Ok(res)
     }
 
-    offsets
+    fn find_offset(data: &Vec<Vec<char>>) -> Option<usize> {
+        for offset in 0..data.len() - 1 {
+            let depth = min(offset + 1, data.len() - offset - 1);
+            let mut diff = 0;
+            for i in 0..depth {
+                diff += zip(&data[offset - i], &data[offset + i + 1])
+                    .map(|(x, y)| if x == y { 0 } else { 1 })
+                    .sum::<usize>();
+            }
+            if diff == 1 {
+                return Some(offset);
+            }
+        }
+        None
+    }
 }
 
-fn check_reflection(s: &Vec<char>, offset: usize) -> bool {
-    let depth = min(offset + 1, s.len() - offset - 1);
-    for i in 0..depth {
-        if s[offset - i] != s[offset + i + 1] {
-            return false;
+mod part1 {
+    use super::*;
+    use std::{cmp::min, collections::HashSet};
+
+    pub fn solve(maps: &Vec<Map>) -> Result<usize, Error> {
+        let mut res: usize = 0;
+
+        for map in maps.iter() {
+            let mut cols: HashSet<usize> = HashSet::new();
+
+            for (i, row) in map.rows().enumerate() {
+                if i == 0 {
+                    cols = find_possible_reflection_offsets(row);
+                } else {
+                    cols.retain(|offset| check_reflection(row, *offset));
+                }
+            }
+
+            for col in cols.iter() {
+                res += col + 1;
+            }
+
+            let mut rows: HashSet<usize> = HashSet::new();
+
+            for (i, col) in map.cols().enumerate() {
+                if i == 0 {
+                    rows = find_possible_reflection_offsets(&col);
+                } else {
+                    rows.retain(|offset| check_reflection(&col, *offset));
+                }
+            }
+
+            for row in rows.iter() {
+                res += (row + 1) * 100;
+            }
+
+            if cols.is_empty() && rows.is_empty() {
+                panic!("no reflection found:\n{}", map);
+            }
+        }
+
+        Ok(res)
+    }
+
+    fn find_possible_reflection_offsets(s: &Vec<char>) -> HashSet<usize> {
+        let mut offsets = HashSet::new();
+
+        for offset in 0..s.len() - 1 {
+            if check_reflection(s, offset) {
+                offsets.insert(offset);
+            }
+        }
+
+        offsets
+    }
+
+    fn check_reflection(s: &Vec<char>, offset: usize) -> bool {
+        let depth = min(offset + 1, s.len() - offset - 1);
+        for i in 0..depth {
+            if s[offset - i] != s[offset + i + 1] {
+                return false;
+            }
+        }
+        true
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_check_reflection() {
+            assert_eq!(check_reflection(&"#.##..##.".chars().collect(), 4), true);
+            assert_eq!(
+                check_reflection(&"..##.###..###.##.".chars().collect(), 0),
+                true
+            );
+        }
+
+        #[test]
+        fn test_find_possible_reflection_offsets() {
+            assert_eq!(
+                find_possible_reflection_offsets(&"#.##..##.".chars().collect()),
+                HashSet::from_iter(vec![4, 6])
+            );
+            assert_eq!(
+                find_possible_reflection_offsets(&"..##.###..###.##.".chars().collect()),
+                HashSet::from_iter(vec![0, 8, 14])
+            );
         }
     }
-    true
 }
 
 #[derive(Debug)]
@@ -101,6 +175,7 @@ impl Map {
     fn new(data: &Vec<Vec<char>>) -> Self {
         let rows = data.len();
         let cols = data.first().unwrap().len();
+
         Map {
             data: data.clone(),
             rows,
@@ -182,32 +257,5 @@ impl From<io::Error> for Error {
 impl From<num::ParseIntError> for Error {
     fn from(value: num::ParseIntError) -> Self {
         Error::ParseInt(value)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-
-    #[test]
-    fn test_check_reflection() {
-        assert_eq!(check_reflection(&"#.##..##.".chars().collect(), 4), true);
-        assert_eq!(
-            check_reflection(&"..##.###..###.##.".chars().collect(), 0),
-            true
-        );
-    }
-
-    #[test]
-    fn test_find_possible_reflection_offsets() {
-        assert_eq!(
-            find_possible_reflection_offsets(&"#.##..##.".chars().collect()),
-            HashSet::from_iter(vec![4, 6])
-        );
-        assert_eq!(
-            find_possible_reflection_offsets(&"..##.###..###.##.".chars().collect()),
-            HashSet::from_iter(vec![0, 8, 14])
-        );
     }
 }
