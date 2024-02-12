beb31b33b59b1f423cc7dc9fcd840c6c4fc9c1d1
Anton Prokhorov <prokhorov.anton.s@gmail.com>
2024-02-12 21:20:28 +0000

    day19: part2

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,4 +1,3 @@
-use core::panic;
 use std::{
     collections::HashMap,
     fs::File,
@@ -13,41 +12,190 @@ fn main() -> Result<()> {
 
     let data = parse(&mut file)?;
 
-    println!("{:?}", solve(&data));
+    println!("{:?}", part1::solve(&data));
+    println!("{:?}", part2::solve(&data));
 
     Ok(())
 }
 
-fn solve(data: &Data) -> u64 {
-    let mut res = 0;
+mod part2 {
+    use super::*;
 
-    'outer: for rating in &data.ratings {
-        if let Some(workflow) = data.workflows.get("in") {
-            let mut workflow_ref = workflow;
+    #[derive(Debug, Copy, Clone)]
+    struct State {
+        x: (u64, u64),
+        m: (u64, u64),
+        a: (u64, u64),
+        s: (u64, u64),
+    }
 
-            while let Some(action) = workflow_ref.apply(rating) {
-                match action {
-                    Action::Accept => {
-                        res += rating.values.values().sum::<u64>();
-                        continue 'outer;
-                    }
-                    Action::Reject => continue 'outer,
-                    Action::Goto(name) => {
-                        if let Some(next_workflow) = data.workflows.get(&name) {
-                            workflow_ref = next_workflow;
+    impl State {
+        fn apply_condition(&self, condition: &Condition) -> Option<State> {
+            let x = self.x;
+            let m = self.m;
+            let a = self.a;
+            let s = self.s;
+
+            match condition.category {
+                Category::X => Some(State {
+                    x: apply_condition(condition, x)?,
+                    m,
+                    a,
+                    s,
+                }),
+                Category::M => Some(State {
+                    x,
+                    m: apply_condition(condition, m)?,
+                    a,
+                    s,
+                }),
+                Category::A => Some(State {
+                    x,
+                    m,
+                    a: apply_condition(condition, a)?,
+                    s,
+                }),
+                Category::S => Some(State {
+                    x,
+                    m,
+                    a,
+                    s: apply_condition(condition, s)?,
+                }),
+            }
+        }
+
+        fn count(&self) -> u64 {
+            (self.x.1 - self.x.0 + 1)
+                * (self.m.1 - self.m.0 + 1)
+                * (self.a.1 - self.a.0 + 1)
+                * (self.s.1 - self.s.0 + 1)
+        }
+    }
+
+    fn apply_condition(condition: &Condition, range: (u64, u64)) -> Option<(u64, u64)> {
+        match condition.sign {
+            Sign::Gt => {
+                if condition.value >= range.1 {
+                    None
+                } else if condition.value > range.0 && condition.value < range.1 {
+                    Some((condition.value + 1, range.1))
+                } else {
+                    Some(range)
+                }
+            }
+            Sign::Lt => {
+                if condition.value <= range.0 {
+                    None
+                } else if condition.value > range.0 && condition.value < range.1 {
+                    Some((range.0, condition.value - 1))
+                } else {
+                    Some(range)
+                }
+            }
+        }
+    }
+
+    fn neg(condition: &Condition) -> Condition {
+        Condition {
+            category: condition.category,
+            sign: match condition.sign {
+                Sign::Gt => Sign::Lt,
+                Sign::Lt => Sign::Gt,
+            },
+            value: match condition.sign {
+                Sign::Gt => condition.value + 1,
+                Sign::Lt => condition.value - 1,
+            },
+        }
+    }
+
+    pub fn solve(data: &Data) -> u64 {
+        let mut res = 0;
+        let start = State {
+            x: (1, 4000),
+            m: (1, 4000),
+            a: (1, 4000),
+            s: (1, 4000),
+        };
+
+        let states = process(start, "in", 0, data);
+
+
+        for state in &states {
+            res += state.count();
+        }
+
+        res
+    }
+
+    fn process(state: State, workflow_name: &str, rule_index: usize, data: &Data) -> Vec<State> {
+        let mut res = Vec::new();
+
+        if let Some(workflow) = data.workflows.get(workflow_name) {
+            let mut current = state;
+            for ri in rule_index..workflow.rules.len() {
+                let rule = &workflow.rules[ri];
+                match rule {
+                    Rule::Unconditional(action) => match action {
+                        Action::Accept => res.push(current.clone()),
+                        Action::Reject => {}
+                        Action::Goto(name) => res.extend(process(current, name, 0, data)),
+                    },
+                    Rule::Conditional(condition, action) => {
+                        if let Some(s) = current.apply_condition(condition) {
+                            match action {
+                                Action::Accept => res.push(s.clone()),
+                                Action::Reject => {}
+                                Action::Goto(name) => res.extend(process(s, name, 0, data)),
+                            }
+                        }
+                        if let Some(next) = current.apply_condition(&neg(condition)) {
+                            current = next;
                         } else {
-                            panic!("invalid workflow");
+                            break;
                         }
                     }
                 }
             }
-        } else {
-            panic!("invalid workflow");
         }
+
+        res
     }
+}
 
+mod part1 {
+    use super::*;
 
-    res
+    pub fn solve(data: &Data) -> u64 {
+        let mut res = 0;
+
+        'outer: for rating in &data.ratings {
+            if let Some(workflow) = data.workflows.get("in") {
+                let mut workflow_ref = workflow;
+
+                while let Some(action) = workflow_ref.apply(rating) {
+                    match action {
+                        Action::Accept => {
+                            res += rating.values.values().sum::<u64>();
+                            continue 'outer;
+                        }
+                        Action::Reject => continue 'outer,
+                        Action::Goto(name) => {
+                            if let Some(next_workflow) = data.workflows.get(&name) {
+                                workflow_ref = next_workflow;
+                            } else {
+                                panic!("invalid workflow");
+                            }
+                        }
+                    }
+                }
+            } else {
+                panic!("invalid workflow");
+            }
+        }
+
+        res
+    }
 }
 
 fn parse<R: Read>(buf: &mut R) -> Result<Data> {
@@ -143,7 +291,7 @@ impl Workflow {
     fn apply(&self, rating: &Rating) -> Option<Action> {
         for rule in &self.rules {
             match rule {
-                Rule::Uncoditional(action) => {
+                Rule::Unconditional(action) => {
                     return Some(action.clone());
                 }
                 Rule::Conditional(condition, action) => {
@@ -161,7 +309,7 @@ impl Workflow {
 #[derive(Debug)]
 enum Rule {
     Conditional(Condition, Action),
-    Uncoditional(Action),
+    Unconditional(Action),
 }
 
 impl FromStr for Rule {
@@ -170,12 +318,12 @@ impl FromStr for Rule {
         if let Some((cond, act)) = s.split_once(':') {
             Ok(Rule::Conditional(cond.parse()?, act.parse()?))
         } else {
-            Ok(Rule::Uncoditional(s.parse()?))
+            Ok(Rule::Unconditional(s.parse()?))
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Condition {
     category: Category,
     sign: Sign,
@@ -232,7 +380,7 @@ impl FromStr for Category {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Sign {
     Gt,
     Lt,
