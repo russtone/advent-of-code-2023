08fcee99a7744846663caab02db3b8cecb9a054d
Anton Prokhorov <prokhorov.anton.s@gmail.com>
2024-02-21 22:19:42 +0000

    day21: part2

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{HashSet, VecDeque},
+    collections::{HashMap, HashSet, VecDeque},
     fs::File,
     io::{BufRead, BufReader, Read},
 };
@@ -11,46 +11,160 @@ fn main() -> Result<()> {
 
     let map = parse(&mut file)?;
     let start = map.find_start().ok_or("no start")?;
-    let mut points: VecDeque<(usize, usize)> = VecDeque::new();
-    let mut seen: HashSet<(usize, usize)> = HashSet::new();
-    points.push_back(start);
 
-    for _ in 0..=64 {
-        seen.clear();
-        let mut count = points.len();
-
-        while count > 0 {
-            count -= 1;
-            if let Some((row, col)) = points.pop_front() {
-                if !seen.insert((row, col)) {
-                    continue;
-                }
-                if row > 0 && map.get_at(row - 1, col) == Some('.') {
-                    points.push_back((row - 1, col))
-                }
-                if map.get_at(row + 1, col) == Some('.') {
-                    points.push_back((row + 1, col))
-                }
-                if col > 0 && map.get_at(row, col - 1) == Some('.') {
-                    points.push_back((row, col - 1))
-                }
-                if map.get_at(row, col + 1) == Some('.') {
-                    points.push_back((row, col + 1))
-                }
-            }
-        }
-    }
-
-    println!("{:?}", seen.len());
+    println!("Part 1: {:?}", part1::solve(&map, start));
+    println!("Part 2: {:?}", part2::solve(&map, start),);
 
     Ok(())
 }
 
-fn show_map(map: &Map, seen: &HashSet<(usize, usize)>) {
+/*
+┌─────┬─────┬──╳──┬─────┬─────┐
+│#####│*****│#╱#╲#│*****│#####│
+│#####│*****│╱###╲│*****│#####│
+│#####│*****╳#####╳*****│#####│
+│#####│****╱│#####│╲****│#####│
+│#####│***╱*│#####│*╲***│#####│
+├─────┼──╳──┼─────┼──╳──┼─────┤
+│*****│#╱###│*****│###╲#│*****│
+│*****│╱####│*****│####╲│*****│
+│*****╳#####│*****│#####╳*****│
+│****╱│#####│*****│#####│╲****│
+│***╱*│#####│*****│#####│*╲***│
+├──╳──┼─────┼─────┼─────┼──╳──┤
+│#╱###│*****│#####│*****│###╲#│
+│╱####│*****│#####│*****│####╲│
+╲#####│*****│#####│*****│#####▲
+│╲####│*****│#####│*****│####╱│
+│#╲###│*****│#####│*****│###╱#│
+├──╳──┼─────┼─────┼─────┼──╳──┤
+│***╲*│#####│*****│#####│*╱***│
+│****╲│#####│*****│#####│╱****│
+│*****╳#####│*****│#####╳*****│
+│*****│╲####│*****│####╱│*****│
+│*****│#╲###│*****│###╱#│*****│
+├─────┼──╳──┼─────┼──╳──┼─────┤
+│#####│***╲*│#####│*╱***│#####│
+│#####│****╲│#####│╱****│#####│
+│#####│*****╳#####╳*****│#####│
+│#####│*****│╲###╱│*****│#####│
+│#####│*****│#╲#╱#│*****│#####│
+└─────┴─────┴──╱──┴─────┴─────┘
+*/
+
+mod part2 {
+
+    use super::*;
+
+    pub fn solve(map: &Map, start: (usize, usize)) -> usize {
+        let mut points: VecDeque<((usize, usize), usize)> = VecDeque::new();
+        let mut seen: HashMap<(usize, usize), usize> = HashMap::new();
+        points.push_back((start, 0));
+
+        while let Some(((row, col), dist)) = points.pop_front() {
+            if seen.contains_key(&(row, col)) {
+                continue;
+            }
+            seen.insert((row, col), dist);
+            if row > 0 && get_at(map, row - 1, col) == Some('.') {
+                points.push_back(((row - 1, col), dist + 1))
+            }
+            if get_at(map, row + 1, col) == Some('.') {
+                points.push_back(((row + 1, col), dist + 1))
+            }
+            if col > 0 && get_at(map, row, col - 1) == Some('.') {
+                points.push_back(((row, col - 1), dist + 1))
+            }
+            if get_at(map, row, col + 1) == Some('.') {
+                points.push_back(((row, col + 1), dist + 1))
+            }
+        }
+
+        let even = seen.values().filter(|d| *d % 2 == 0).count();
+        let odd = seen.values().filter(|d| *d % 2 == 1).count();
+
+        let half = map.cols / 2;
+        assert!(half == 65);
+
+        assert!((26501365 - half) % map.cols == 0);
+
+        let n = (26501365 - half) / map.cols;
+
+        let even_corners = seen
+            .iter()
+            .filter(|&(&(row, col), dist)| {
+                dist % 2 == 0 && half.abs_diff(row) + half.abs_diff(col) > 65
+            })
+            .count();
+
+        let odd_corners = seen
+            .iter()
+            .filter(|&(&(row, col), dist)| {
+                dist % 2 == 1 && half.abs_diff(row) + half.abs_diff(col) > 65
+            })
+            .count();
+
+        let res = (n + 1) * (n + 1) * odd + n * n * even - (n + 1) * odd_corners + n * even_corners;
+
+        res
+    }
+}
+
+mod part1 {
+    use super::*;
+
+    pub fn solve(map: &Map, start: (usize, usize)) -> usize {
+        let mut points: VecDeque<(usize, usize)> = VecDeque::new();
+        let mut seen: HashSet<(usize, usize)> = HashSet::new();
+        points.push_back(start);
+
+        for _ in 0..=64 {
+            seen.clear();
+            let mut count = points.len();
+
+            while count > 0 {
+                count -= 1;
+                if let Some((row, col)) = points.pop_front() {
+                    if !seen.insert((row, col)) {
+                        continue;
+                    }
+                    if row > 0 && get_at(map, row - 1, col) == Some('.') {
+                        points.push_back((row - 1, col))
+                    }
+                    if get_at(map, row + 1, col) == Some('.') {
+                        points.push_back((row + 1, col))
+                    }
+                    if col > 0 && get_at(map, row, col - 1) == Some('.') {
+                        points.push_back((row, col - 1))
+                    }
+                    if get_at(map, row, col + 1) == Some('.') {
+                        points.push_back((row, col + 1))
+                    }
+                }
+            }
+        }
+
+        seen.len()
+    }
+}
+
+fn get_at(map: &Map, row: usize, col: usize) -> Option<char> {
+    if row > map.rows - 1 || col > map.cols - 1 {
+        return None;
+    }
+
+    if map.data[row][col] == 'S' {
+        return Some('.');
+    }
+
+    Some(map.data[row][col])
+}
+
+fn show_map(map: &Map, seen: &HashMap<(usize, usize), usize>) {
     println!();
     for i in 0..map.rows {
         for j in 0..map.cols {
-            if seen.contains(&(i, j)) {
+            if seen.contains_key(&(i, j)) {
                 print!("O");
             } else {
                 print!("{}", map.data[i][j]);
@@ -85,17 +199,5 @@ impl Map {
             }
         }
         None
-    }
-
-    fn get_at(&self, row: usize, col: usize) -> Option<char> {
-        if row > self.rows - 1 || col > self.cols - 1 {
-            return None;
-        }
-
-        if self.data[row][col] == 'S' {
-            return Some('.');
-        }
-
-        Some(self.data[row][col])
     }
 }
