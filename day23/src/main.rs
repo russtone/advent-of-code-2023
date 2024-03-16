e48f36ee30d1841439be1524355495e82c3ffc0f
Anton Prokhorov <prokhorov.anton.s@gmail.com>
2024-03-16 23:09:06 +0000

    day23: part2
    Second part is adoptation of: https://gist.github.com/qwewqa/00d8272766c2945f4aa965ea36dba7f5

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
-    cmp::Ordering,
-    collections::{BinaryHeap, HashMap, HashSet},
+    cmp::{max, Ordering},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap,  VecDeque},
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -13,12 +13,78 @@ fn main() -> Result<()> {
 
     let map = Map::new(lines.map(|line| line.unwrap().chars().collect()).collect());
 
-    println!("Part1: {:?}", part1(&map)?);
+    println!("Part1: {:?}", part1(&map));
+    println!("Part2: {:?}", part2(&map));
 
     Ok(())
 }
 
-fn part1(map: &Map) -> Result<usize> {
+fn part2(map: &Map) -> usize {
+    let mut edges = map.edges();
+
+    loop {
+        let mut to_modify = None;
+
+        for (&p, neighbors) in edges.iter() {
+            if neighbors.len() == 2 {
+                let mut iter = neighbors.iter();
+                let e1 = iter.next().unwrap().clone();
+                let e2 = iter.next().unwrap().clone();
+                to_modify = Some((p, e1, e2));
+                break;
+            }
+        }
+
+        if let Some((p, e1, e2)) = to_modify {
+            if let Some(s) = edges.get_mut(&e1.0) {
+                s.remove(&(p, e1.1));
+                s.insert((e2.0, e1.1 + e2.1));
+            }
+            if let Some(s) = edges.get_mut(&e2.0) {
+                s.remove(&(p, e2.1));
+                s.insert((e1.0, e1.1 + e2.1));
+            }
+            edges.remove(&p);
+        } else {
+            break;
+        }
+    }
+
+    let mut queue = VecDeque::new();
+    let mut seen: BTreeSet<Point> = BTreeSet::new();
+    let mut res = 0;
+
+    queue.push_back((map.start, 0));
+
+    while let Some((p, d)) = queue.pop_back() {
+        if d == 0 && p != map.start {
+            seen.remove(&p);
+            continue;
+        }
+
+        if p == map.end {
+            res = max(res, d);
+            continue;
+        }
+
+        if seen.contains(&p) {
+            continue;
+        }
+
+        seen.insert(p);
+        queue.push_back((p, 0));
+
+        if let Some(s) = edges.get(&p) {
+            s.iter().for_each(|e| {
+                queue.push_back((e.0, e.1 + d));
+            });
+        }
+    }
+
+    res
+}
+
+fn part1(map: &Map) -> usize {
     let mut max_id = 0;
 
     let mut next_id = || {
@@ -50,7 +116,6 @@ fn part1(map: &Map) -> Result<usize> {
 
             let id = if i == 0 { cur_node.id } else { next_id() };
 
-
             let next_node = Node::new(*next_p, id);
             came_from.insert(next_node, cur_node);
 
@@ -62,11 +127,11 @@ fn part1(map: &Map) -> Result<usize> {
         }
     }
 
-    Ok((0..=max_id)
+    (0..=max_id)
         .into_iter()
         .map(|id| reconstruct_path(&came_from, &Node::new(map.end, id)).len() - 1)
         .max()
-        .unwrap())
+        .unwrap()
 }
 
 fn reconstruct_path(came_from: &HashMap<Node, Node>, start: &Node) -> Vec<Node> {
@@ -220,6 +285,36 @@ impl Map {
             }
             print!("\n");
         }
+    }
+
+    fn edges(&self) -> BTreeMap<Point, BTreeSet<(Point, usize)>> {
+        let mut edges: BTreeMap<Point, BTreeSet<(Point, usize)>> = BTreeMap::new();
+
+        for row in 0..self.rows {
+            for col in 0..self.cols {
+                let ch = self.data[row][col];
+                if ch == '.' || ch == '>' || ch == 'v' {
+                    let p = Point::new(row, col);
+                    for np in self.get_next(&p, false).iter() {
+                        edges
+                            .entry(p)
+                            .and_modify(|s| {
+                                s.insert((*np, 1));
+                            })
+                            .or_insert(BTreeSet::from_iter(vec![(*np, 1)]));
+
+                        edges
+                            .entry(*np)
+                            .and_modify(|s| {
+                                s.insert((p, 1));
+                            })
+                            .or_insert(BTreeSet::from_iter(vec![(p, 1)]));
+                    }
+                }
+            }
+        }
+
+        edges
     }
 }
 
